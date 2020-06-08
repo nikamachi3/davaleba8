a9c7d9536615c8b71d79d8f6785f490b7e848c3d
nikamachi3 <55693519+nikamachi3@users.noreply.github.com>
2020-06-08 22:08:54 +0400

    Add files via upload

---
+++ Students/Students/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Students
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            XDocument xmlDocument = new XDocument(
+                new XDeclaration("1.0", "utf-8", "yes"),
+                new XComment("Vqmni XML DOkuments"),
+                new XElement("Students",
+                    new XElement("s1",
+                        new XElement("Firstname", "Nika"),
+                        new XElement("Lastname", "Darchiashvili"),
+                        new XElement("ID", "098402198201")),
+                    new XElement("s2",
+                        new XElement("Firstname", "Vaso"),
+                        new XElement("Lastname", "Nasyidashvili"),
+                        new XElement("ID", "03583959939")),
+                    new XElement("s3",
+                        new XElement("Firstname", "Elene"),
+                        new XElement("Lastname", "Burjanadze"),
+                        new XElement("ID", "49503090935"))
+                        ));
+            xmlDocument.Save("C:\\Users\\Y\\Desktop\\Students\\students.xml");
+            Console.WriteLine("Saved");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Students/XmlRead/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XmlRead
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //XmlDocument xdoc = new XmlDocument();
+            string file = "C:\\Users\\Y\\Desktop\\Students\\students.xml";
+            //xdoc.Load(file);
+            //xdoc.Save(Console.Out);          
+            XmlReader reader = XmlReader.Create(file);
+            while (reader.Read())
+            {
+                if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "s1"))
+                {
+                    if (reader.HasAttributes)
+                    {
+                        Console.WriteLine("ai aq" + reader.GetAttribute("Firstname"));
+
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
